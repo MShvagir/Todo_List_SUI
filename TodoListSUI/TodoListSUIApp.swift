33dc33a2386de22c096fa24f3a9021add44a7db7
Marina Shvagir <alira_m@mail.ru>
2022-07-01 00:04:05 +0300

    Initial Commit

---
+++ TodoListSUI/TodoListSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSUIApp.swift
+//  TodoListSUI
+//
+//  Created by Марина Швагирь on 01.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
